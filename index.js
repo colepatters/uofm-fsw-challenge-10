d2b1a097d926c113ccd87f8a021df41c405a61e1
Cole Patterson <cole.patter@gmail.com>
2024-07-29 18:48:29 -0500

    install jest and inquirer, set up color keywords

---
+++ index.js
@@ -0,0 +1,14 @@
+const inqiurer = require('inquirer')
+
+const colorKeywords = {
+    "red": "#f54242",
+    "green": "#42f575",
+    "blue": "#424ef5",
+
+    "pink": "#f542b6",
+    "purple": "#b342f5",
+    "yellow": "#f5e642",
+    "orange": "#f59042",
+    "teal": "#42f5d7"
+}
+
