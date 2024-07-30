6e6e788d8486ba00f35c589c5ab3d59c079d01e1
Cole Patterson <cole.patter@gmail.com>
2024-07-29 19:12:05 -0500

    questions complete

--- index.js
+++ index.js
@@ -12,3 +12,92 @@ const colorKeywords = {
     "teal": "#42f5d7"
 }
 
+const shapes = {
+    circle: "circle.svg",
+    triangle: "triangle.svg",
+    square: "square.svg"
+}
+
+const welcomeText = `
+Welcome to logo generator! Let's get started. Here's a quick summary of how this works.
+1. Enter text to be displayed on your shape
+2. Choose the text's color (keyword or hex)
+3. Choose a shape
+4. Pick the shape's color (keyword or hex)
+
+Once complete, you can find your generated shape in the root folder.
+Sound good?`
+
+const questions = [
+    // intro
+    {
+        type: "confirm",
+        message:welcomeText
+    },
+    // shape text (3 char max)
+    {
+        type: "input",
+        name: "text",
+        message: "Enter up to 3 characters to be displayed on your shape.",
+        validate: (answer) => answer.length < 4
+    },
+    
+    // text color (keyword or hex)
+    {
+        type: "list",
+        name: "textColorType",
+        message: "(Text Color) Would you like to choose from a list of pre-defined colors, or enter your own?",
+        choices: [ "keyword", "hex" ]
+    },
+    // text color (keyword)
+    {
+        type: "list",
+        name: "textColorKeyword",
+        message: "Choose a color keyword.",
+        choices: Object.keys(colorKeywords),
+        when: (answers) => answers.textColorType === "keyword"
+    },
+    // text color (hex)
+    {
+        type: "input",
+        name: "textColorHex",
+        message: "Enter the color hex value. (including leading #)",
+        validate: (answer) => (answer.includes("#") && answer.length === 7),
+        when: (answers) => answers.textColorType === "hex"
+    },
+
+    // shape
+    {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape.",
+        choices: Object.keys(shapes)
+    },
+    // shape color (keyword or hex)
+    {
+        type: "list",
+        name: "shapeColorType",
+        message: "(Shape Color) Would you like to choose from a list of pre-defined colors, or enter your own?",
+        choices: [ "keyword", "hex" ]
+    },
+    // shape color (keyword)
+    {
+        type: "list",
+        name: "shapeColorKeyword",
+        message: "Choose a color keyword.",
+        choices: Object.keys(colorKeywords),
+        when: (answers) => answers.shapeColorType === "keyword"
+    },
+    // shape color (hex)
+    {
+        type: "input",
+        name: "shapeColorHex",
+        message: "Enter the color hex value. (including leading #)",
+        validate: (answer) => (answer.includes("#") && answer.length === 7),
+        when: (answers) => answers.shapeColorType === "hex"
+    },
+]
+
+inqiurer.default.prompt(questions).then((answers) => {
+    console.log(answers)
+})
